3d52c9a04089deec65d8fa49fa659c8dcbd75f2b
derric-d <derric.donehoo@gmail.com>
2020-09-29 20:30:00 +0000

    add puppet script

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# strace on apache process running on user www-data with curl for localhost
+# showed no file present named '...<.phpp>' indicating typo
+
+exec { 'sed php line':
+	command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+	path => "/bin"
+}
