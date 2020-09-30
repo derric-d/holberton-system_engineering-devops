3470dae1dd6765b4dd22fc89673b5147954e2bda
derric-d <derric.donehoo@gmail.com>
2020-09-30 18:10:45 +0000

    puppet lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 # showed no file present named '...<.phpp>' indicating typo
 
 exec { 'sed php line':
-  command => 'sed -i 's/phpp/php/g' /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => '/bin'
 }
